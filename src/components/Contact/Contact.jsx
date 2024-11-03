8a701e101cc849489666b5612a618f8278921a3c
Daryna Kurytsyna <darina.kuritsina@gmail.com>
2024-11-03 23:23:52 +0100

    update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -4,13 +4,15 @@ import { FaUserLarge, FaPhone } from 'react-icons/fa6';
 const Contact = ({ name, number, id, onDeleteProfile }) => {
   return (
     <>
-      <div className={css.box}>
-        <FaUserLarge />
-        <span className={css.formInput}>{name}</span>
-      </div>
-      <div className={css.box}>
-        <FaPhone />
-        <span className={css.formInput}>{number}</span>
+      <div>
+        <div className={css.box}>
+          <FaUserLarge />
+          <span className={css.formInput}>{name}</span>
+        </div>
+        <div className={css.box}>
+          <FaPhone />
+          <span className={css.formInput}>{number}</span>
+        </div>
       </div>
       <button type="button" onClick={() => onDeleteProfile(id)}>
         Delete
